42914ff6d61ff45c7d5d637ea33d943f2bc616bb
usman12321 <usiddique351@gmail.com>
2024-03-13 15:19:36 +0500

    Diagnosis Popover Completed

--- components/patient-problem-editable.tsx
+++ components/patient-problem-editable.tsx
@@ -1,11 +1,20 @@
 import { Check, Edit, Edit2, Trash, Trash2 } from "lucide-react";
+import {
+  DiagnosisDialog,
+  DiagnosisDialogTrigger,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+} from "./ui/diagnosis-dialog";
+import { Button } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
 
 const PatientProblemsEditable = () => {
   return (
     <div className="p-4 bg-white rounded-xl mb-4 w-[450px]">
       <div className="flex justify-between">
         <div className="">
-          <h5 className="text-sm font-semibold">Problem</h5>
+          <h5 className="text-lg font-semibold">Problem</h5>
           <h3 className="text-xl font-bold my-2">Toothache</h3>
           <h4 className="text-[#21B9C6] text-lg font-semibold">
             Treatment Options
@@ -14,7 +23,140 @@ const PatientProblemsEditable = () => {
         <div className="flex flex-col items-end gap-2 text-right">
           <div className="text-right flex gap-x-2">
             <Trash2 className="text-red-600" />
-            <Edit className="text-[#21B9C6]" />
+            <DiagnosisDialog>
+              <DiagnosisDialogTrigger asChild>
+                <Edit className="text-[#21B9C6]" />
+              </DiagnosisDialogTrigger>
+              <DialogContent className="bg-white text-black">
+                <DialogHeader>
+                  <DialogDescription className="flex flex-col  gap-1 overflow-y-auto">
+                    <div>
+                      <div className="flex justify-between items-center">
+                        <div>
+                          <h5 className="text-lg font-semibold">Problem</h5>
+                          <h3 className="text-xl font-bold my-2">Toothache</h3>
+                          <h4 className="text-[#21B9C6] text-lg font-semibold">
+                            Treatment Options
+                          </h4>
+                        </div>
+                        <Button
+                          variant="outline"
+                          className="text-lg py-5 px-8 rounded-2xl border-2 font-semibold"
+                        >
+                          Reversible Pulpitis
+                        </Button>
+                      </div>
+                      <div>
+                        <input
+                          placeholder="Enter diagnosis"
+                          className="border-2 rounded-3xl w-full px-3 py-2 text-medium my-2"
+                        />
+                        <div className="space-y-3">
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Removal of causative agent
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Filling
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Repair of existing restoration
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Continue to monitor symptoms
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Visit dentist if symptoms persist or worsen
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Removal of causative agent
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Filling
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Repair of existing restoration
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Continue to monitor symptoms
+                            </label>
+                          </div>
+                          <div className="flex items-center space-x-2 border-b-2 border-b-opacity-5 py-2">
+                            <Checkbox id="treatment" />
+                            <label
+                              htmlFor="treatment"
+                              className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Visit dentist if symptoms persist or worsen
+                            </label>
+                          </div>
+                        </div>
+                        <div className="flex justify-center my-5">
+                          <Button
+                            variant="login"
+                            className="px-16 py-7 text-lg rounded-full"
+                          >
+                            Done
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </DiagnosisDialog>
           </div>
 
           <h4 className="text-lg font-semibold">Reversible pulpitis</h4>
