531f2f086ab9182b437348762a625746a54e51dc
UsmanDev09 <usiddique09@gmail.com>
2024-04-15 15:42:44 +0500

    React konva polygon integrated

---
+++ app/(dashboard)/(routes)/react-konva/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import dynamic from 'next/dynamic';
+
+const Canvas = dynamic(() => import('../../../../components/Canvas'), {
+  ssr: false,
+});
+
+export default function Page(props: any) {
+  return <Canvas />;
+}
