826d7f6c641fbd72cf287639cbf76919472ad6fe
UsmanDev09 <usiddique09@gmail.com>
2024-04-29 19:23:40 +0500

    toast removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { ClipboardListIcon, Filter, Search } from "lucide-react";
 import AnalyticalCard from "@/components/analyticsCard";
-import { ToastContainer, toast } from 'react-toastify';
 
 import {
   Table,
@@ -57,7 +56,6 @@ export default async function Home() {
 
   return (
       <div className="flex flex-col gap-y-10 p-10 bg-[#F5F5F5]">
-        <ToastContainer />
         <div className="flex flex-row justify-between items-center flex-wrap">
           <AnalyticalCard />
           <AnalyticalCard />
